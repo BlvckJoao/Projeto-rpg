6e6fbeb7980f16f1590731a9dfb906a112ef2db4
BL4K4TH <166950387+BL4K4TH@users.noreply.github.com>
2024-10-22 12:05:12 -0300

    DEU CERTO
    AAAEEEEEEEEEE

---
+++ main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include "rpgtools.h"
+
+void clear() {
+	
+	//Verifica qual é o sistema operacional para utilizar o comando de limpar a tela correto
+	#ifdef _WIN32
+		system("cls");
+	#else
+		system("clear");
+	#endif
+	
+}
+
+void printtxt(char nome[]) {
+	
+	//Abre o arquivo para leitura
+	FILE *arquivo = fopen(nome, "r");
+	
+	//Verifica se o arquivo foi aberto (se não, mostra uma mensagem de erro)
+	if(arquivo == NULL) {
+		printf("Erro ao procurar o arquivo %s\n", nome);
+		printf("Verifique se o arquivo está presente na pasta do jogo.");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Imprime todas as linhas presentes no arquivo e pula uma linha
+	char linha[256];
+	while(fgets(linha, sizeof(linha), arquivo) != NULL) {
+		printf("%s", linha);
+	}
+	printf("\n");
+	
+	//Fecha o arquivo
+	fclose(arquivo);
+	
+}
+
+int main() {
+	
+	//Definindo os caracteres para português
+	setlocale(LC_ALL, "Portuguese");
+	
+	//Váriaveis da lógica do programa
+	int loop = 1;
+	char escolha[20];
+	
+	//Váriavel do personagem
+	tpersonagem personagem;
+	
+	//Tela principal
+	while(loop) {
+		clear();
+		printtxt("Logo.txt");
+		printf("\n");
+		printf("Bem-vindo ao nosso RPG de texto! O que deseja fazer?\n");
+		printf("\n");
+		printf("[jogar]\n");
+		printf("[sair] do programa\n");
+		printf("\n");
+		printf("Digite a palavra em parênteses: ");
+		scanf("%s", escolha);
+		printf("\n");
+		if(strcmp(escolha, "jogar") == 0) {
+			
+			// Menu de criação e personagem
+			clear();
+			personagem = criar_personagem();
+			
+		} else if(strcmp(escolha, "sair") == 0) {
+			
+			//Fim do programa
+			clear();
+			printf("Adeus! Espero te ver novamente :)\n\n");
+			system("PAUSE");
+			loop = 0;
+		}
+	}
+}
---
+++ rpgtools.h
@@ -0,0 +1,99 @@
+#ifndef RPGTOOLS_H
+#define RPGTOOLS_H
+
+#define MAX 100
+
+typedef enum{
+    HUMANO,
+    ELFO,
+    ANAO,
+    SHEDER,
+    BERSERKIR,
+    MERMEDE,
+}traca;
+
+typedef enum{
+    GUERREIRO,
+    LADINO,
+    MAGO,
+    BARBARO,
+    BARDO,
+    CLERIGO,
+    ENGENHEIRO,
+    ARQUEIRO,
+}tclasse;
+
+typedef enum{
+    HUMANOIDE,
+    ANIMAL,
+    MONSTRO,
+}tbixo;
+
+typedef enum{
+   ARMA,
+   ARMADURA,
+   CONSUMIVEL,
+}tclasseitem;
+
+typedef struct{
+    int forca;
+    int agilidade;
+    int vigor;
+    int intelecto;
+    int carisma;
+    int sabedoria;
+}tstats;
+
+//ficha para itens
+
+typedef struct{
+    char nome[20];
+    tclasseitem tipo;
+    int efeito;
+    int peso;
+}titem;
+
+//ficha para personagem
+
+typedef struct{
+    char nome[MAX];
+    int level;
+    float experiencia;
+    traca raca_pers;
+    tclasse classe_pers;
+    tstats status_pers;
+    titem inventario[100];
+    titem arma, armadura;
+    int max_inventario;
+    int vida, max_vida;
+    int mana, max_mana;
+}tpersonagem;
+
+//ficha para entidades nao jogaveis (inimigos, npcs, etc)
+
+typedef struct{
+     char nome[MAX];
+     tbixo bixo_ent;
+     int vida;
+     tstats stats_ent;
+}tentidade;
+
+tpersonagem criar_personagem();
+
+void MostrarInventario(tpersonagem personagem);
+
+void PegarItem(tpersonagem *personagem, titem item);
+
+void UsarItem(tpersonagem *personagem, int item_escolhido);
+
+void LargarItemFora(int item_escolhido);
+
+int random_num(int intervalo);
+
+int attack(int dano, int atk, int defesa);
+
+int fugir(tpersonagem *personagem, tentidade entidade);
+
+void combate(tpersonagem personagem, tentidade entidade);
+
+#endif
